a4f86e7326d2d40a20bef658c4bf6ef9c3201ebe
CarterJ6243 <71455111+CarterJ6243@users.noreply.github.com>
2020-09-20 15:19:48 -0700

    Update sketch.js

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -17,15 +17,21 @@ function draw(){
 	    balls[i].drawBall();
       balls[i].moveBall();
 	  }
+    for (let i=0; i < hats.length; i++){
+      hats[i].drawHat();
+      hats[i].moveHat();
+    }
+
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-if (keyCode === ENTER);
+
   let  b = new Ball(random(0,800),0);
   balls.push(b);
   console.log(balls);
-  if(keyCode === SHIFT);
-  let
+  let hats = new Hat(0,300);
+  hats.push(h);
+  console.log(hats);
 
 }
 
@@ -39,12 +45,11 @@ class Ball {
 
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(255,255,255);
-    		//fill(random(0,270), random(0,270), random(0,270));
         ellipse(this.x,this.y,random(10,20),random(10,20));
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
-    this.x = this.x;
+    this.x = this.x+.25;
 		this.y = this.y+2;
 	}
 
@@ -60,8 +65,8 @@ class Hat {
 
     drawHat(){
     stroke(0)
-    fill(255,random(0,270),0)
-    rect(this.x,this.y,this.x+20,this.y+20)
+    fill("red")
+    rect(this.x,this.y,20,20)
   }
   moveHat(){
     this.x = this.x+4
